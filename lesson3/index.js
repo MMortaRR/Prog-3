7073ecf5933730bc48317981d83da5962c4852df
MMortaRR <edgar.g2.y@tumo.org>
2019-06-09 15:19:27 +0400

    hi

--- lesson3/index.js
+++ lesson3/index.js
@@ -1,18 +1,30 @@
-
 var express = require("express");
 var app = express();
+/*
+app.get("/",function(req,res){
+    res.send("hello gev jan");
+});
 
-app.get("/google/:search", function (req, res) {
+app.get("/name/:name",function(req,res){
+    var name = req.params.name;
+    res.send("<h1>Hello " + name + " jan</h1>")
+});
+
+app.get("/google",function(req,res){
+    res.redirect("http://google.com");
+});
+
+app.get("/google/:search",function(req,res){
     var search = req.params.search;
-    res.redirect("https://www.google.com/search?q=" + search);
+    res.redirect("http://google.com/search?q="+search)
 });
 
-app.get("/*", function (req, res) {
-    res.send("error 404");
-});
+app.get("/*",function(req,res){
+    res.send("Error 404");
+});*/
 
-app.listen(3000, function () {
-    console.log("Example is running on port 3000");
+app.listen(3000,function(){
+    console.log("example is running on port 3000");
 });
 
 

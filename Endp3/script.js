967ea6214ed5b8b6328f8f71b28101bb78167f2d
MMortaRR <edgar.g2.y@tumo.org>
2019-06-12 17:03:39 +0400

    hi

---
+++ Endp3/script.js
@@ -0,0 +1,44 @@
+function setup() {
+
+    var socket = io();
+
+    var side = 30;
+
+    var matrix = [];
+
+    let grassCountElement = document.getElementById('grassCount');
+
+    socket.on("data", drawCreatures);
+
+    function drawCreatures(data) {
+        matrix = data.matrix;
+        grassCountElement.innerText = data.grassCounter;
+      
+        createCanvas(matrix[0].length * side, matrix.length * side)
+       
+        background('#acacac');
+        for (var i = 0; i < matrix.length; i++) {
+            for (var j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 1) {
+                    fill("green");
+                    rect(j * side, i * side, side, side);
+                } else if (matrix[i][j] == 2) {
+                    fill("orange");
+                    rect(j * side, i * side, side, side);
+                } else if (matrix[i][j] == 0) {
+                    fill('#acacac');
+                    rect(j * side, i * side, side, side);
+                } else if (matrix[i][j] == 3) {
+                    fill('red');
+                    rect(j * side, i * side, side, side);
+                } else if (matrix[i][j] == 4) {
+                    fill('blue');
+                    rect(j * side, i * side, side, side);
+                } else if (matrix[i][j] == 5) {
+                    fill('yellow');
+                    rect(j * side, i * side, side, side);
+                }
+            }
+        }
+    }
+}
